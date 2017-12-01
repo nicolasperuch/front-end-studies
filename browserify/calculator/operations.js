ac3c849f6e34668389f8e774d3110831c303f110
Nicolas Peruch <nicolasperuch.2010@gmail.com>
2017-12-01 17:05:33 -0200

    Calculator exporting operations from a class

---
+++ browserify/calculator/operations.js
@@ -0,0 +1,15 @@
+exports.sum = function (firstValue, secondValue) {
+    return firstValue + secondValue;
+};
+
+exports.sub = function (firstValue, secondValue) {
+    return firstValue - secondValue;
+};
+
+exports.multiply = function (firstValue, secondValue) {
+    return firstValue * secondValue;
+};
+
+exports.division = function (firstValue, secondValue) {
+    return firstValue / secondValue;
+};
