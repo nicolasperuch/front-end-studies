25ebdea299de52f024b297c6c0c7e35384744cd9
Nicolas Peruch <nicolasperuch.2010@gmail.com>
2018-03-31 21:01:42 -0300

    Learning angular: Getting started done.

---
+++ angular/rank-heroes/angular-tour-of-heroes/src/app/mock-heroes.ts
@@ -0,0 +1,8 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+    { id: 1, name: "Ragnar", history: "Nordic King", image: "https://menshairstylesclub.com/wp-content/uploads/2017/06/ragnar-lothbrok.png"},
+    { id: 2, name: 'Captain Teemo', history: '1, 2, 3, 4.', image:'https://yt3.ggpht.com/a-/AJLlDp1AW0pw-qbbQnPHDFJh_7WJaxjbzvXKDwqlxQ=s900-mo-c-c0xffffffff-rj-k-no' },
+    { id: 3, name: 'Bjorn', history: 'Ironside', image:'https://vignette.wikia.nocookie.net/vikingstv/images/e/e2/Bjorn_HD_5x10.jpg/revision/latest?cb=20180126054329' },
+    { id: 4, name: 'Squirtle', history: '\"escuero escuero\", Squirtle', image:'https://assets.pokemon.com/assets/cms2/img/pokedex/full/007.png' }
+];
