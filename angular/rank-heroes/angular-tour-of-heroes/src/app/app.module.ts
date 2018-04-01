f18d3f44fff9e0286cefd9ce46b7544375e73579
Nicolas Peruch <nicolasperuch.2010@gmail.com>
2018-04-01 17:58:48 -0300

    Fixing app.module

--- angular/rank-heroes/angular-tour-of-heroes/src/app/app.module.ts
+++ angular/rank-heroes/angular-tour-of-heroes/src/app/app.module.ts
@@ -4,7 +4,6 @@ import { FormsModule } from '@angular/forms';
 
 
 import { AppComponent } from './app.component';
-import { HeroesComponent } from './heroes/heroes.component';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 import { HeroService } from './service/hero.service';
 import { AppRoutingModule } from './/app-routing.module';
@@ -14,7 +13,6 @@ import { DashboardComponent } from './dashboard/dashboard.component';
 @NgModule({
   declarations: [
     AppComponent,
-    HeroesComponent,
     HeroDetailComponent,
     DashboardComponent
   ],
